e1173638ab2b7d5a6661205b0dda14b8a752f9af
Muhammed-Haider <iamhaider072@gmail.com>
2024-09-17 10:47:42 +0500

    python intro project added

---
+++ 00_intro_python/age_riddle.py
@@ -0,0 +1,16 @@
+def main():
+    Anton:int=21
+    Beth:int=Anton+6
+    Chen:int=Beth+20
+    Drew:int=Chen+Anton
+    Ethan:int=Chen
+
+
+    print(f' Anton {Anton}\n Beth {Beth} \n Chen {Chen} \n Drew {Drew} \n Ethan {Ethan}')
+
+
+
+
+
+if __name__=='__main__':
+    main()
---
+++ 00_intro_python/square_number.py
@@ -0,0 +1,11 @@
+def main():
+    prompt:str=input("Enter a number that you want the square of : ")
+    square:int=int(prompt)**2
+    print(f"Square of {prompt} is {square}")
+
+
+
+
+
+if __name__=='__main__':
+      main()
